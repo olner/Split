9bde21b3085fe1024b4cc285f742681e579ced73
olner <izmailov2003@yandex.ru>
2023-04-30 18:21:38 +0300

    base exceptions

---
+++ Split/Split.Engine/Exceptions/UserNotFoundException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Split.Engine.Exceptions
+{
+    internal class UserNotFoundException : ServiceException
+    {
+        public UserNotFoundException() : base("User not found")
+        {
+        }
+    }
+}
